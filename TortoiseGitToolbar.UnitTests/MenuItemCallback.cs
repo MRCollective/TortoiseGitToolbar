fe91b1c31c0b1e998c41f7ca832d9cfef1499098
Matt Davies <matt@mdavies.net>
2013-08-11 20:52:30 +0800

    Cleanup unit tests project

---
+++ TortoiseGitToolbar.UnitTests/MenuItemCallback.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.Design;
+using System.Reflection;
+using MattDavies.TortoiseGitToolbar;
+using MattDavies.TortoiseGitToolbar.Config.Constants;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VsSDK.UnitTestLibrary;
+using TortoiseGitToolbar.UnitTests.Helpers;
+
+namespace TortoiseGitToolbar.UnitTests
+{
+    [TestClass]
+    public class MenuItemTest
+    {
+        [TestMethod]
+        public void InitializeMenuCommand()
+        {
+            var package = new TortoiseGitToolbarPackage() as IVsPackage;
+            var serviceProvider = OleServiceProvider.CreateOleServiceProviderWithBasicServices();
+
+            var menuCommandID = new CommandID(PackageConstants.guidTortoiseGitToolbarCmdSet, (int)CommandIdConstants.CmdCommit);
+            var info = typeof(Package).GetMethod("GetService", BindingFlags.Instance | BindingFlags.NonPublic);
+            var mcs = info.Invoke(package, new object[] { (typeof(IMenuCommandService)) }) as OleMenuCommandService;
+
+            Assert.IsNotNull(package, "The object does not implement IVsPackage");
+            Assert.AreEqual(0, package.SetSite(serviceProvider), "SetSite did not return S_OK");
+            Assert.IsNotNull(info);
+            Assert.IsNotNull(mcs.FindCommand(menuCommandID));
+        }
+
+        [TestMethod]
+        public void MenuItemCallback()
+        {
+            var package = new TortoiseGitToolbarPackage() as IVsPackage;
+            var info = package.GetType().GetMethod("MenuItemCallback", BindingFlags.Instance | BindingFlags.NonPublic);
+            Assert.IsNotNull(info, "Failed to get the private method MenuItemCallback through reflection");
+            var serviceProvider = OleServiceProvider.CreateOleServiceProviderWithBasicServices();
+            var uishellMock = UIShellServiceMock.GetUiShellInstance();
+            
+            serviceProvider.AddService(typeof(SVsUIShell), uishellMock, true);
+            info.Invoke(package, new object[] { null, null });
+            serviceProvider.RemoveService(typeof(SVsUIShell));
+
+            Assert.IsNotNull(package, "The object does not implement IVsPackage");
+            Assert.AreEqual(0, package.SetSite(serviceProvider), "SetSite did not return S_OK");
+        }
+    }
+}
--- TortoiseGitToolbar.UnitTests/PackageTest.cs
+++ TortoiseGitToolbar.UnitTests/PackageTest.cs
@@ -1,51 +1,35 @@
-﻿/***************************************************************************
-
-Copyright (c) Microsoft Corporation. All rights reserved.
-This code is licensed under the Visual Studio SDK license terms.
-THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
-ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
-IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
-PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
-
-***************************************************************************/
-
-using MattDavies.TortoiseGitToolbar;
+﻿using MattDavies.TortoiseGitToolbar;
 using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.VsSDK.UnitTestLibrary;
 
 namespace TortoiseGitToolbar.UnitTests
 {
-    [TestClass()]
+    [TestClass]
     public class PackageTest
     {
-        [TestMethod()]
+        [TestMethod]
         public void CreateInstance()
         {
-            TortoiseGitToolbarPackage package = new TortoiseGitToolbarPackage();
+            var package = new TortoiseGitToolbarPackage();
         }
 
-        [TestMethod()]
+        [TestMethod]
         public void IsIVsPackage()
         {
-            TortoiseGitToolbarPackage package = new TortoiseGitToolbarPackage();
-            Assert.IsNotNull(package as IVsPackage, "The object does not implement IVsPackage");
+            var package = new TortoiseGitToolbarPackage();
+            Assert.IsNotNull(package, "The object does not implement IVsPackage");
         }
 
-        [TestMethod()]
+        [TestMethod]
         public void SetSite()
         {
-            // Create the package
-            IVsPackage package = new TortoiseGitToolbarPackage() as IVsPackage;
+            var package = new TortoiseGitToolbarPackage() as IVsPackage;
+            
+            var serviceProvider = OleServiceProvider.CreateOleServiceProviderWithBasicServices();
+            
             Assert.IsNotNull(package, "The object does not implement IVsPackage");
-
-            // Create a basic service provider
-            OleServiceProvider serviceProvider = OleServiceProvider.CreateOleServiceProviderWithBasicServices();
-
-            // Site the package
             Assert.AreEqual(0, package.SetSite(serviceProvider), "SetSite did not return S_OK");
-
-            // Unsite the package
             Assert.AreEqual(0, package.SetSite(null), "SetSite(null) did not return S_OK");
         }
     }
