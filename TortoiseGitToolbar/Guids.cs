9a16faec420b7c41598e51a2c6f0b2582717e64a
Matt Davies <matt@mdavies.net>
2013-08-11 19:27:34 +0800

    Initial commit (VSPackage template)

---
+++ TortoiseGitToolbar/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace MattDaviesmdaviesnet.TortoiseGitToolbar
+{
+    static class GuidList
+    {
+        public const string guidTortoiseGitToolbarPkgString = "f388ee16-eef2-4ae1-85bd-4cb19151beb0";
+        public const string guidTortoiseGitToolbarCmdSetString = "b594f95f-5d4d-4383-be94-e21d105fa58c";
+
+        public static readonly Guid guidTortoiseGitToolbarCmdSet = new Guid(guidTortoiseGitToolbarCmdSetString);
+    };
+}
---
+++ TortoiseGitToolbar/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace MattDaviesmdaviesnet.TortoiseGitToolbar
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdToolbar =        0x100;
+
+
+    };
+}
