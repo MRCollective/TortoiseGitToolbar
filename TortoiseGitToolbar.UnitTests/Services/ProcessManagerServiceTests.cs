7faff177a2f18a204d0f06947181da2dad4cd112
Matt Davies <matt@mdavies.net>
2013-08-14 18:47:11 +0800

    Minor test refactor

--- TortoiseGitToolbar.UnitTests/Services/ProcessManagerServiceTests.cs
+++ TortoiseGitToolbar.UnitTests/Services/ProcessManagerServiceTests.cs
@@ -6,6 +6,9 @@ namespace TortoiseGitToolbar.UnitTests.Services
     [TestFixture]
     public class ProcessManagerServiceTests
     {
+        private const string TestFileName = "testFileName";
+        private const string TestArguments = "testArguments";
+        private const string TestWorkingDirectory = "testWorkingDirectory";
         private ProcessManagerService _processManagerService;
 
         [SetUp]
@@ -17,28 +20,21 @@ namespace TortoiseGitToolbar.UnitTests.Services
         [Test]
         public void Get_process_with_working_directory()
         {
-            const string fileName = "testFileName";
-            const string arguments = "testArguments";
-            const string workingDirectory = "testWorkingDirectory";
+            var process = _processManagerService.GetProcess(TestFileName, TestArguments, TestWorkingDirectory);
 
-            var process = _processManagerService.GetProcess(fileName, arguments, workingDirectory);
-
-            Assert.That(process.FileName, Is.EqualTo(fileName));
-            Assert.That(process.Arguments, Is.EqualTo(arguments));
-            Assert.That(process.WorkingDirectory, Is.EqualTo(workingDirectory));
+            Assert.That(process.FileName, Is.EqualTo(TestFileName));
+            Assert.That(process.Arguments, Is.EqualTo(TestArguments));
+            Assert.That(process.WorkingDirectory, Is.EqualTo(TestWorkingDirectory));
             Assert.That(process.UseShellExecute, Is.False);
         }
 
         [Test]
         public void Get_process_without_working_directory()
         {
-            const string fileName = "testFileName";
-            const string arguments = "testArguments";
+            var process = _processManagerService.GetProcess(TestFileName, TestArguments);
 
-            var process = _processManagerService.GetProcess(fileName, arguments);
-
-            Assert.That(process.FileName, Is.EqualTo(fileName));
-            Assert.That(process.Arguments, Is.EqualTo(arguments));
+            Assert.That(process.FileName, Is.EqualTo(TestFileName));
+            Assert.That(process.Arguments, Is.EqualTo(TestArguments));
             Assert.That(process.WorkingDirectory, Is.EqualTo(string.Empty));
             Assert.That(process.UseShellExecute, Is.True);
         }
