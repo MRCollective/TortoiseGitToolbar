778bb012aab4c46812dfd55a39e2bb8eed4b4793
ccHanibal <oliver.daum@codecentric.de>
2017-01-07 12:50:32 +0100

    Fix for #2 and #10. Paths for TortoiseGit and git bash are retrieved from registry.

---
+++ TortoiseGitToolbar.UnitTests/Services/PathConfigurationTests.cs
@@ -0,0 +1,25 @@
+﻿using MattDavies.TortoiseGitToolbar.Config.Constants;
+using NUnit.Framework;
+
+namespace TortoiseGitToolbar.UnitTests.Services
+{
+    [TestFixture]
+    public class PathConfigurationTests
+    {
+        [Test, Explicit("Works only if TortoiseGit installed.")]
+        public void CanGetTortoiseProcPathFromRegistry()
+        {
+            var path = PathConfiguration.GetTortoiseGitPathFromRegistry();
+            Assert.That(path, Is.Not.Null);
+            Assert.That(path, Is.StringEnding("TortoiseGitProc.exe").IgnoreCase);
+        }
+
+        [Test, Explicit("Works only if TortoiseGit installed.")]
+        public void CanGetGitBashPathFromRegistry()
+        {
+            var path = PathConfiguration.GetGitBashPathFromRegistry();
+            Assert.That(path, Is.Not.Null);
+            Assert.That(path, Is.StringEnding("sh.exe").IgnoreCase);
+        }
+    }
+}
