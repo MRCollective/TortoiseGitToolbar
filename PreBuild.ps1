2641db70b61953e9a7a52cb85c60a2a3412efac8
Rob Moore <me@robdmoore.id.au>
2020-07-13 22:49:59 +0800

    Adding PreBuild.ps1 for appveyor build
    Pulling out TortoiseGitToolbar specific stuff out of https://github.com/MRCollective/AppVeyorConfig/blob/master/appveyor.yml

---
+++ PreBuild.ps1
@@ -0,0 +1,16 @@
+choco install -y --no-progress tortoisegit
+
+#Registry key for sh.exe isn't created until this runs once (causing a failing test)
+. "C:\Program Files\TortoiseGit\bin\TortoiseGitProc.exe"
+
+Write-Output "Waiting for all active installations to finish..."
+do {
+
+  $msitest = Get-Process -ProcessName msiexec -ErrorAction SilentlyContinue
+  if ($msitest) {
+    Start-Sleep -Seconds 60
+  }
+
+}
+until (!$msitest)
+Write-Output "Verified all installations have finished."
