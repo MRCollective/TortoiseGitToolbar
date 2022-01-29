85ac71f56b3dfaa4fb7ec4c799b56cfdfda1ca06
Matt Davies <matt@mdavies.net>
2022-01-29 20:34:48 +1100

    Create an additional backwards compatible VS2019 and earlier VSIX

---
+++ TortoiseGitToolbar2019/PackageConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+
+namespace MattDavies.TortoiseGitToolbar.Config.Constants
+{
+    public static class PackageConstants
+    {
+        public const string GuidTortoiseGitToolbarPkgString = "d09f7b88-0165-4ec2-94da-cdb0c9fc914a";
+        public const string GuidTortoiseGitToolbarCmdSetString = "46533cec-75d7-43bf-bfbb-910b527b9b4a";
+
+        public static readonly Guid GuidTortoiseGitToolbarCmdSet = new Guid(GuidTortoiseGitToolbarCmdSetString);
+        public static readonly Guid GuidTortoiseGitToolbarPkg = new Guid(GuidTortoiseGitToolbarPkgString);
+    };
+}
